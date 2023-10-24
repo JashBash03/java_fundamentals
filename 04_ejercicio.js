fdb356198db848bd72e97558dc76d60472842451
Javi <javiercamarerolopez@gmail.com>
2023-10-24 18:39:37 +0200

    Añado el ejercicio 04

---
+++ 04_ejercicio.js
@@ -0,0 +1,43 @@
+function sumar()
+{
+    let x = 2
+    let y = 1
+
+    return x + y
+}
+console.log(sumar())
+
+function restar()
+{
+    let x = 2
+    let y = 1
+    
+    return x - y
+}
+console.log(restar())
+
+function multuplicar()
+{
+    let x = 2
+    let y = 1
+    
+    return x * y
+}
+console.log(multuplicar())
+
+function dividir()
+{
+    let x = 2
+    let y = 1
+    
+    return x / y
+}
+console.log(dividir())
+
+function doble()
+{
+    let x = 2
+    
+    return x * 2
+}
+console.log(doble())
