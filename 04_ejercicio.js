8d7dbcc014571910e38647f212c019ed94fd0714
Javi <javiercamarerolopez@gmail.com>
2023-10-24 19:37:41 +0200

    Comprimo el código

--- 04_ejercicio.js
+++ 04_ejercicio.js
@@ -1,43 +1,32 @@
+let x = 2
+let y = 1
+
 function sumar()
 {
-    let x = 2
-    let y = 1
-
     return x + y
 }
 console.log(sumar())
 
 function restar()
 {
-    let x = 2
-    let y = 1
-    
     return x - y
 }
 console.log(restar())
 
 function multuplicar()
 {
-    let x = 2
-    let y = 1
-    
     return x * y
 }
 console.log(multuplicar())
 
 function dividir()
 {
-    let x = 2
-    let y = 1
-    
     return x / y
 }
 console.log(dividir())
 
 function doble()
 {
-    let x = 2
-    
     return x * 2
 }
 console.log(doble())
