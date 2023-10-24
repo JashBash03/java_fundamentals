0c3df1820b763d455d3dae7ad5d92aac604962ee
Javi <javiercamarerolopez@gmail.com>
2023-10-24 14:58:31 +0200

    Arreglo el ejecicio

--- 03_ejercicio.js
+++ 03_ejercicio.js
@@ -4,6 +4,5 @@ let usuario =
     edad: 20,
     ciudad: "Granada"
 }
-var saludo = "Hola, me llamo " + usuario.nombre + ", tengo " + usuario.edad + " años y soy de " + usuario.ciudad
-
+var saludo = `Hola, me llamo ${usuario.nombre}, tengo ${usuario.edad} años y soy de ${usuario.ciudad}`
 console.log(saludo);
