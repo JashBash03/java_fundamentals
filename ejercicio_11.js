d1973ca14e90307dbc439c66f476f66032085ebe
Javi <javiercamarerolopez@gmail.com>
2023-11-07 14:52:32 +0100

    Añado ejercicio 11

---
+++ ejercicio_11.js
@@ -0,0 +1,10 @@
+let lista = [1, 0.1, "Hola", false]
+for (let i = 0; i < lista.length; i++){
+    console.log(lista[i], typeof lista[i])
+}
+
+function iterarArray(arr){
+    for (let i = 0; i < arr.length; i++){
+        console.log(arr[i], typeof arr[i])
+    }
+}
