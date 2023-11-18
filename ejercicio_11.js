457d18f28e0dc197d5289ef0b293e4f245beccb5
Javi <javiercamarerolopez@gmail.com>
2023-11-18 12:15:10 +0100

    corrijo ejercicio 11

--- ejercicio_11.js
+++ ejercicio_11.js
@@ -1,10 +1,4 @@
 let lista = [1, 0.1, "Hola", false]
-for (let i = 0; i < lista.length; i++){
-    console.log(lista[i], typeof lista[i])
-}
-
-function iterarArray(arr){
-    for (let i = 0; i < arr.length; i++){
-        console.log(arr[i], typeof arr[i])
-    }
+for (let i = 0; i < lista.length; i++) {
+    console.log(`Valor: ${lista[i]}, Tipo de dato: ${typeof lista[i]}`);
 }
