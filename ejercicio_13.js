d81688e0bd971f39c82c2d85bd106c1fc23f9b86
Javi <javiercamarerolopez@gmail.com>
2023-11-30 16:44:16 +0100

    Fix ejercicio 13

--- ejercicio_13.js
+++ ejercicio_13.js
@@ -1,6 +1,7 @@
-function sumarArrayNumeros(array) {
-    let lista = [1, 2, 3, 4, 5]
-    let resultado = lista.reduce((acumulador, elemento) => acumulador + elemento)
-    console.log(resultado)
+function sumaElementos(arr){
+    let resultado = 0;
+    for (let i = 0; i < arr.length; i++) {
+        resultado += arr[i];
     }
-    const resultadoNum = sumarArrayNumeros();
+    return resultado;
+}
